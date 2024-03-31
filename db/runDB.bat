11d662b338387b34362be8bee66601942df72e90
zakhiyaharsal <zakhiyah.arsal@akr.co.id>
2024-03-31 21:54:15 +0700

    upload DB for testing purpose
    upload db for testing purpose

---
+++ db/runDB.bat
@@ -0,0 +1,5 @@
+title HSQLDBServer
+java  -cp "D:\Work_Directory\GIT_Upload\BookingLocker\db\lib\hsqldb.jar" org.hsqldb.server.Server --database.0 file:database\Locker --dbname.0 locker
+pause
+
+:: D:\Work_Directory\GIT_Upload\BookingLocker\db\lib\hsqldb.jar => rubah sesuai letak directory file hsqldb.jar berada
